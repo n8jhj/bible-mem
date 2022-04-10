7f3797b2de86ae9560e5ba6c5535319015496904
Nathaniel Jones <n8jhj@live.com>
2022-04-09 23:25:34 -0700

    Incorporate Translation into Verse
    Verse now includes translation data.

--- bible_mem/constructs/verse.py
+++ bible_mem/constructs/verse.py
@@ -1,10 +1,17 @@
 from .reference import Reference
+from .translation import Translation
 
 
 class Verse:
-    def __init__(self, reference: Reference, text: str):
+    def __init__(self, reference: Reference, translation: Translation, text: str):
         self.reference = reference
+        self.translation = translation
         self.text = text
 
     def __repr__(self):
-        return f"Verse<{self.reference.readout}>"
+        return (
+            f"Verse<"
+            f"{self.reference.readout} "
+            f"({self.translation.abbreviation}; {self.translation.year_published})"
+            f">"
+        )
--- test/test_verse.py
+++ test/test_verse.py
@@ -1,8 +1,12 @@
-from bible_mem.constructs import Book, Reference, Verse
+from bible_mem.constructs import Book, Reference, Translation, Verse
 
 
 def test_verse():
     text = "Jesus Christ is the same yesterday and today and forever."
-    verse = Verse(Reference(Book("Hebrews"), 13, 8), text)
-    assert repr(verse) == "Verse<He. 13:8>"
+    verse = Verse(
+        Reference(Book("Hebrews"), 13, 8),
+        Translation("New International Version", 2011),
+        text,
+    )
+    assert repr(verse) == "Verse<He. 13:8 (NIV; 2011)>"
     assert verse.text == text
