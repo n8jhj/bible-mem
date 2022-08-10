3421083536de53024cf15080f13816a0f1a7dfad
Nathaniel Jones <n8jhj@live.com>
2022-08-09 22:36:03 -0700

    Add code to try out difflib

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from bible_mem.app import main_loop
+# from bible_mem.app import main_loop
+from bible_mem.difflib_try import difflib_try
 
 
 if __name__ == "__main__":
-    main_loop()
+    # main_loop()
+    difflib_try()
